c236a69291830a3eb2ef917679c81c7b1e32f3a6
zxbgoat <teslasloan@zju.edu.cn>
2024-02-02 23:37:30 +0800

    create the repo

---
+++ transformer.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+
+class LayerNorm:
+    pass
+
+
+class MultiHeadSelfAttn:
+    pass
+
+
+class Encoder:
+    
+    def __init__(self, layer_num, layer_dim) -> None:
+        pass
+
+
+class Decoder:
+    pass
+
+
+class Transformer:
+
+    def __init__(self, layer_num, layer_dim) -> None:
+        pass
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
+
+
+if __name__ == "__main__":
+    pass
