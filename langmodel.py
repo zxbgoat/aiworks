0d20229ccd8f5cb37b42ae5b6da62a05117ff3b5
zxbgoat <teslasloan@zju.edu.cn>
2024-02-16 10:04:14 +0800

    add translator for ipynb

--- langmodel.py
+++ langmodel.py
@@ -1,17 +1,103 @@
-from torch import nn
-from torch import Tensor
-from torch.nn import TransformerEncoder
-from torch.nn import TransformerEncoderLayer
+import math
+import os
+from tempfile import TemporaryDirectory
+from typing import Tuple
+
+import torch
+from torch import nn, Tensor
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import dataset
 
+from torchtext.datasets import WikiText2
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
 
-class LanguageModel(nn.Module):
+
+class TransformerModel(nn.Module):
 
     def __init__(self,
-                 num_token:  int,
-                 dim_model:  int,
-                 num_head:   int,
-                 dim_hidden: int,
-                 num_layer:  int,
-                 dropout:    float = 0.5):
-        pass
+                 ntoken: int,
+                 d_model: int,
+                 nhead: int, d_hid: int,
+                 nlayers: int,
+                 dropout: float = 0.5
+                ) -> None:
+        super().__init__()
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.embedding = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+        self.linear = nn.Linear(d_model, ntoken)
+        self.init_weights()
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src: Tensor, src_mask: Tensor = None) -> Tensor:
+        """
+        Arguments:
+            src: Tensor, shape ``[seq_len, batch_size]``
+            src_mask: Tensor, shape ``[seq_len, seq_len]``
+
+        Returns:
+            output Tensor of shape ``[seq_len, batch_size, ntoken]``
+        """
+        src = self.embedding(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        if src_mask is None:
+            """Generate a square causal mask for the sequence. The masked positions are filled with float('-inf').
+            Unmasked positions are filled with float(0.0).
+            """
+            src_mask = nn.Transformer.generate_square_subsequent_mask(len(src)).to(device)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.linear(output)
+        return output
+
+
+def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+    """Converts raw text into a flat Tensor."""
+    data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
+    return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
+
+
+def batchify(data: Tensor, bsz: int, device) -> Tensor:
+    """Divides the data into ``bsz`` separate sequences, removing extra elements
+    that wouldn't cleanly fit.
+
+    Arguments:
+        data: Tensor, shape ``[N]``
+        bsz: int, batch size
+
+    Returns:
+        Tensor of shape ``[N // bsz, bsz]``
+    """
+    seq_len = data.size(0) // bsz
+    data = data[:seq_len * bsz]
+    data = data.view(bsz, seq_len).t().contiguous()
+    return data.to(device)
+
+
+if __name__ == '__main__':
+    train_iter = WikiText2(split='train')
+    tokenizer = get_tokenizer('basic_english')
+    vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
+    vocab.set_default_index(vocab['<unk>'])
+
+    # ``train_iter`` was "consumed" by the process of building the vocab,
+    # so we have to create it again
+    train_iter, val_iter, test_iter = WikiText2('/home/tesla/dataset/wikitext-2.zip')
+    train_data = data_process(train_iter)
+    val_data = data_process(val_iter)
+    test_data = data_process(test_iter)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    batch_size = 20
+    eval_batch_size = 10
+    train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
+    val_data = batchify(val_data, eval_batch_size)
+    test_data = batchify(test_data, eval_batch_size)
